16a0e4f00bbbcc1418e7c1e6d396b186248e8d88
antonioggeorgiev <antonio.g.georgiev1998@gmail.com>
2024-08-05 22:38:19 +0300

    Changed the logic for protected routes

--- auth.config.ts
+++ auth.config.ts
@@ -7,8 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isProtectedPage =
-        nextUrl.pathname !== "/sign-up" && nextUrl.pathname !== "/sign-in";
+      const isProtectedPage = nextUrl.pathname === "/";
       if (isProtectedPage) {
         if (isLoggedIn) return true;
         return false;
