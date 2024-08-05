ffff5ba37f83750cbad0c4c121a68eb502da2350
antonioggeorgiev <antonio.g.georgiev1998@gmail.com>
2024-08-06 01:14:42 +0300

    Reverted the notifications page to be the default one

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,12 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isProtectedPage = nextUrl.pathname === "/notifications";
+      const isProtectedPage = nextUrl.pathname === "/";
       if (isProtectedPage) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/notifications", nextUrl));
+        return Response.redirect(new URL("/", nextUrl));
       }
       return true;
     },
