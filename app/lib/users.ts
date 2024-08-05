d052ee047b3575ae0838cf18a9d344f17c0bec4c
antonioggeorgiev <antonio.g.georgiev1998@gmail.com>
2024-08-05 22:18:02 +0300

    Added missing getUsers function

---
+++ app/lib/users.ts
@@ -0,0 +1,19 @@
+import { PrismaClient, User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export async function getUser(email: string): Promise<User | null> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
